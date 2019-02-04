937378fb578e2d65e3e869f8fbd05f2cdc463b3d
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-02-04 15:50:11 -0800

    revised purchase model and model test

--- test/models/Purchase.test.js
+++ test/models/Purchase.test.js
@@ -4,6 +4,8 @@ const Purchase = require('../../lib/models/Purchase');
 const Store = require('../../lib/models/Store');
 const Customer = require('../../lib/models/Customer');
 const { Types, connection } = require('mongoose');
+const Chance = require('chance');
+const chance  = new Chance();
 
 const createStore = name => {
   return Store.create({
@@ -13,7 +15,12 @@ const createStore = name => {
   })
     .then(store => ({ ...store, _id: store._id.toString() }));
 };
-const createCustomer = 
+const createCustomer = () => {
+  return Customer.create({
+    name: chance.name()
+  })
+    .then(customer => customer);
+};
 
 
 
@@ -29,17 +36,22 @@ describe('test purchase model', () => {
   });
 
   it('validates a good purchase model', () => {
-    return createStore('Raskin Bobbins')
-      .then(store => {
-        const purchase = new Purchase({
+    return Promise.all([
+      createCustomer(),
+      createStore('Raskin Bobbins')
+    ])
+      .then(([customer, store]) => {
+        const purchase  = new Purchase({
           product: 'cone',
           price: 5.00,
-          store: store._id
+          store: store._id,
+          customer: customer._id
         });
         expect(purchase.toJSON()).toEqual({
           product: 'cone',
           price: 5.00,
           store: expect.any(Types.ObjectId),
+          customer: expect.any(Types.ObjectId),
           _id: expect.any(Types.ObjectId)
         });
       });
