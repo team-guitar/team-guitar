0dc76b782c0aef9aff251110b95864e8485ec24f
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-02-04 11:04:45 -0800

    Store model created and passes the first test validating a store model with requied fields

---
+++ test/models/Store.test.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
+const Store = require('../../lib/models/Store');
+const { Types, connection } = require('mongoose');
+
+describe('test store model', () => {
+  beforeEach(done => {
+    return connection.dropDatabase(() => {
+      done();
+    });
+  });
+  afterAll((done) => {
+    connection.close(done);
+  });
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  });
+
+  it('can validate a Store model', () => {
+    const store = new Store({
+      products: ['cone', 'cone2'],
+      address: '301 NW 10th Ave'
+    });
+    expect(store.toJSON()).toEqual({
+      products: ['cone', 'cone2'],
+      address: '301 NW 10th Ave',
+      _id: expect.any(Types.ObjectId)
+    });
+  });
+});
