12c09c227898ed6819dfa2b87e37a225b3176c38
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-04 16:04:13 -0800

    failing test for customer post method written

--- test/routes/customer.test.js
+++ test/routes/customer.test.js
@@ -1,25 +1,44 @@
-// require('dotenv').config();
-// require('../../lib/utils/connect')();
-// const { connection } = require('mongoose');
-// const request = require('supertest');
-// const app = require('../../lib/app');
-// // const Customer = require('../../lib/models/Customer');
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const { connection } = require('mongoose');
+const request = require('supertest');
+const app = require('../../lib/app');
+const Customer = require('../../lib/models/Customer');
+const Chance = require('chance');
+const chance  = new Chance();
 
-// describe('test customer routes', () => {
-//   beforeEach(done => {
-//     return connection.dropDatabase(() => {
-//       done();
-//     });
-//   });
-//   afterAll((done) => {
-//     connection.close(done);
-//   });
+const createCustomer = () => {
+  return Customer.create({
+    name: chance.name()
+  })
+    .then(customer => customer);
+};
 
-//   it('can post a new customer to the DB', () => {
-//     return request(app)
-//       .post('/customer')
-//       .send({
 
-//       });
-//   });
-// });
+describe('test customer routes', () => {
+  beforeEach(done => {
+    return connection.dropDatabase(() => {
+      done();
+    });
+  });
+  afterAll((done) => {
+    connection.close(done);
+  });
+
+  it('can post a new customer to the DB', () => {
+    return createCustomer()
+      .then(customer => {
+        return request(app)
+          .post('/')
+          .send(customer);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: expect.any(String),
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+
+  });
+});
