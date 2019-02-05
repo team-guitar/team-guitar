d2029b3b3e5208901105df177c703089cef98c66
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-02-05 11:35:24 -0800

    seed data loads properly

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ require('./lib/utils/connect')();
 
 const app = require('./lib/app');
 
-const PORT = 27017 || process.env.PORT;
+const PORT = 2000 || process.env.PORT;
 
 
 
 
 
 // eslint-disable-next-line no-console
-app.listen(27017, () => console.log(`Listening on ${PORT}`));
+app.listen(2000, () => console.log(`Listening on ${PORT}`));
